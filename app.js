d183929aeaad4c31a68271a8ee7c64120429c148
Natali Queiroz <queiroznatt@gmail.com>
2024-09-07 19:17:58 -0400

    Configurando Banco de Dados e Tornando Público os Arquivos HTML de Home, Listagem e Cadastro

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+const sqlite3 = require('sqlite3').verbose()
+
+// const db = new sqlite3.Database(':memory:')
+const db = new sqlite3.Database('banco-de-dados.db')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
